92cf536d8dabd87749cbd7729094a59aa4ad602d
Ak5h4yVengala <139264838+Ak5h4yVengala@users.noreply.github.com>
2023-07-11 23:42:12 +0530

    CodeForces 844 Round (Div1+Div2)

---
+++ CF1844B.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define forn(i, a, b) for (int i = (a); i <= (b); ++i)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define sortallr(x) sort(allr(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+//=======================
+const int MOD = 1'000'000'007;
+const int N = 2e6 + 13, M = N;
+//=======================
+// vi g[N];
+// int a[N];
+// int n, m, k;
+//=======================
+
+bool comparator(const pair<int, int> &a,
+                const pair<int, int> &b)
+{
+    return (a.second < b.second);
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n <= 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    vector<int> ans(n);
+    ans[n / 2] = 1;
+    int cur = 4;
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (ans[i] == 0)
+        {
+            ans[i] = cur;
+            cur++;
+        }
+    }
+    ans[0] = 2;
+    ans[n - 1] = 3;
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+void takeinput()
+{
+    int n;
+    cin >> n;
+    vi arr;
+    fo(i, n)
+    {
+        int m;
+        cin >> m;
+        arr.push_back(m);
+    }
+}
